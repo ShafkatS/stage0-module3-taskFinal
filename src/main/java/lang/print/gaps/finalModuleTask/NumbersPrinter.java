7dd25ee6bbbf8f779e7da2b06cd5071b7859e59f
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 20:42:06 +0500

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -4,7 +4,6 @@ public class NumbersPrinter {
     public void printIsPositive(int number) {
         boolean POSITIVE_NUMBER_IS_TRUE = number > 0;
         boolean OTHERWISE_NUMBER_IS_FALSE = number < 0;
-        System.out.println(POSITIVE_NUMBER_IS_TRUE);
         System.out.println(OTHERWISE_NUMBER_IS_FALSE);
 
     }
