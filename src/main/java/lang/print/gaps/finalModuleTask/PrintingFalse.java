b6b3cd75cfdeefadecff8f8d0fa17a3c765e68c7
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 20:35:28 +0500

    Update PrintingFalse.java

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.finalModuleTask;
 
 public class PrintingFalse {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int first = 9;
         int second = -17;
         boolean isFalse = first < second;
